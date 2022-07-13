78ff0a415e86cfc49e73fb2fbb245f84f1716e36
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-07-13 12:41:35 +0800

    change: Fix a bug caused by fixing a bug

--- src/main/java/me/flashyreese/mods/commandaliases/command/builder/reassign/ClientReassignCommandBuilder.java
+++ src/main/java/me/flashyreese/mods/commandaliases/command/builder/reassign/ClientReassignCommandBuilder.java
@@ -2,16 +2,14 @@ package me.flashyreese.mods.commandaliases.command.builder.reassign;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import me.flashyreese.mods.commandaliases.CommandAliasesProvider;
 import me.flashyreese.mods.commandaliases.command.CommandAlias;
 import me.flashyreese.mods.commandaliases.command.CommandMode;
 import me.flashyreese.mods.commandaliases.command.CommandType;
-import me.flashyreese.mods.commandaliases.command.Scheduler;
 import me.flashyreese.mods.commandaliases.command.builder.custom.ClientCustomCommandBuilder;
-import me.flashyreese.mods.commandaliases.storage.database.AbstractDatabase;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 
 import java.lang.reflect.Field;
-import java.util.Map;
 
 /**
  * Represents a client reassign command builder
@@ -23,8 +21,8 @@ import java.util.Map;
  */
 public class ClientReassignCommandBuilder extends AbstractReassignCommandBuilder<FabricClientCommandSource> {
 
-    public ClientReassignCommandBuilder(CommandAlias command, Field literalCommandNodeLiteralField, Map<String, String> reassignClientCommandMap, AbstractDatabase<byte[], byte[]> database, Scheduler scheduler) {
-        super(command, literalCommandNodeLiteralField, reassignClientCommandMap, CommandType.CLIENT, database, scheduler);
+    public ClientReassignCommandBuilder(CommandAlias command, Field literalCommandNodeLiteralField, CommandAliasesProvider commandAliasesProvider) {
+        super(command, literalCommandNodeLiteralField, commandAliasesProvider, CommandType.CLIENT);
     }
 
     @Override
@@ -32,10 +30,10 @@ public class ClientReassignCommandBuilder extends AbstractReassignCommandBuilder
         if (this.reassignCommand(this.command, dispatcher)) {
             String command = this.command.getReassignCommand().getCommand().trim();
             String reassignTo = this.command.getReassignCommand().getReassignTo().trim();
-            this.reassignCommandMap.put(command, reassignTo);
+            this.commandAliasesProvider.getReassignedCommandMap().put(command, reassignTo);
 
             if (this.command.getCommandMode() == CommandMode.COMMAND_REASSIGN_AND_CUSTOM) {
-                return new ClientCustomCommandBuilder(this.command.getCustomCommand(), this.database, this.scheduler).buildCommand(dispatcher);
+                return new ClientCustomCommandBuilder(this.command.getCustomCommand(), this.commandAliasesProvider).buildCommand(dispatcher);
             }
         }
         return null;
