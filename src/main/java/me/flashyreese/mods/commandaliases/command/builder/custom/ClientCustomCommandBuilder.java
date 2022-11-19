4969b15e49fbb0410dc1dd0396961f0a8b12cf6d
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-20 04:55:25 +0800

    change: Fix send command client

--- src/main/java/me/flashyreese/mods/commandaliases/command/builder/custom/ClientCustomCommandBuilder.java
+++ src/main/java/me/flashyreese/mods/commandaliases/command/builder/custom/ClientCustomCommandBuilder.java
@@ -33,7 +33,7 @@ public class ClientCustomCommandBuilder extends AbstractCustomCommandBuilder<Fab
     protected int dispatcherExecute(CustomCommandAction action, CommandDispatcher<FabricClientCommandSource> dispatcher, CommandContext<FabricClientCommandSource> context, String actionCommand) throws CommandSyntaxException {
         int state = 0;
         if (action.getCommandType() == CommandType.CLIENT) {
-            context.getSource().getPlayer().sendChatMessage("/" + actionCommand);
+            context.getSource().getPlayer().sendCommand("/" + actionCommand);
             state = Command.SINGLE_SUCCESS;
         } else if (action.getCommandType() == CommandType.SERVER) {
             state = Objects.requireNonNull(context.getSource().getWorld().getServer()).getCommandManager().getDispatcher().execute(actionCommand, Objects.requireNonNull(context.getSource().getWorld().getServer()).getCommandSource());
