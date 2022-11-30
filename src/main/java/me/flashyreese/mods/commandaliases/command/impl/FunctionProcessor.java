951acd0206ae8582a4e2cca445c892cf69cd8793
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-30 14:07:53 +0800

    change: Add `get_dimension` and fix `get_world``

--- src/main/java/me/flashyreese/mods/commandaliases/command/impl/FunctionProcessor.java
+++ src/main/java/me/flashyreese/mods/commandaliases/command/impl/FunctionProcessor.java
@@ -88,7 +88,7 @@ public class FunctionProcessor<S extends CommandSource> {
             }
             return null;
         });
-        this.functionMap.put("get_dimension", (commandSource, input) -> {
+        this.functionMap.put("get_world", (commandSource, input) -> {
             if (commandSource instanceof ServerCommandSource serverCommandSource) {
                 Optional<ServerPlayerEntity> optionalPlayer = serverCommandSource.getWorld().getPlayers().stream()
                         .filter(serverPlayerEntity -> serverPlayerEntity.getEntityName().equals(input)).findFirst();
@@ -100,6 +100,22 @@ public class FunctionProcessor<S extends CommandSource> {
                         .filter(clientPlayerEntity -> clientPlayerEntity.getEntityName().equals(input)).findFirst();
                 if (optionalPlayer.isPresent()) {
                     return optionalPlayer.get().getEntityWorld().getRegistryKey().getValue().toString();
+                }
+            }
+            return null;
+        });
+        this.functionMap.put("get_dimension", (commandSource, input) -> {
+            if (commandSource instanceof ServerCommandSource serverCommandSource) {
+                Optional<ServerPlayerEntity> optionalPlayer = serverCommandSource.getWorld().getPlayers().stream()
+                        .filter(serverPlayerEntity -> serverPlayerEntity.getEntityName().equals(input)).findFirst();
+                if (optionalPlayer.isPresent()) {
+                    return optionalPlayer.get().getEntityWorld().getDimension().effects().toString();
+                }
+            } else if (commandSource instanceof FabricClientCommandSource clientCommandSource) {
+                Optional<AbstractClientPlayerEntity> optionalPlayer = clientCommandSource.getWorld().getPlayers().stream()
+                        .filter(clientPlayerEntity -> clientPlayerEntity.getEntityName().equals(input)).findFirst();
+                if (optionalPlayer.isPresent()) {
+                    return optionalPlayer.get().getEntityWorld().getDimension().effects().toString();
                 }
             }
             return null;
