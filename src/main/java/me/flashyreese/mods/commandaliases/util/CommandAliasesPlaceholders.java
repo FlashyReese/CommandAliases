5485d9d8850c44a8c93d9c653511a1b738102e4f
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-03-29 00:10:42 +0800

    change: Cleanup placeholders implementation

--- src/main/java/me/flashyreese/mods/commandaliases/util/CommandAliasesPlaceholders.java
+++ src/main/java/me/flashyreese/mods/commandaliases/util/CommandAliasesPlaceholders.java
@@ -4,21 +4,15 @@ import eu.pb4.placeholders.api.PlaceholderResult;
 import eu.pb4.placeholders.api.Placeholders;
 import me.flashyreese.mods.commandaliases.command.impl.FunctionProcessor;
 import me.flashyreese.mods.commandaliases.command.loader.ServerCommandAliasesProvider;
-import net.minecraft.command.CommandSource;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
-
-import java.util.function.BiFunction;
 
 import static me.flashyreese.mods.commandaliases.CommandAliasesMod.MOD_ID;
 
 public class CommandAliasesPlaceholders {
     public static void register(ServerCommandAliasesProvider serverCommandAliasesProvider) {
-        FunctionProcessor functionProcessor = new FunctionProcessor(serverCommandAliasesProvider);
+        FunctionProcessor<ServerCommandSource> functionProcessor = new FunctionProcessor<>(serverCommandAliasesProvider);
 
-        functionProcessor.getFunctionMap().forEach((key, value) -> {
-            String keyOriginal = (String) key;
-            BiFunction<CommandSource, String, String> valueOriginal = (BiFunction<CommandSource, String, String>) value;
-            Placeholders.register(new Identifier(MOD_ID, keyOriginal), ((context, argument) -> PlaceholderResult.value(valueOriginal.apply(context.source(), argument))));
-        });
+        functionProcessor.getFunctionMap().forEach((key, value) -> Placeholders.register(new Identifier(MOD_ID, key), ((context, argument) -> PlaceholderResult.value(value.apply(context.source(), argument)))));
     }
 }
